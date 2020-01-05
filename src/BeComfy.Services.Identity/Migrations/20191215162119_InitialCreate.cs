fbb7655836921c4b00df4ca0f111b643254ebedf
serfin <gracjanbrylka@gmail.com>
2020-01-05 16:55:38 +0100

    Added migrations to repo

---
+++ src/BeComfy.Services.Identity/Migrations/20191215162119_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeComfy.Services.Identity.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Firstname = table.Column<string>(nullable: true),
+                    Secondname = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
